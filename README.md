7a19f05ebf0f731a9e325296793172c6838574eb
Diego <policarpodamae@gmail.com>
2023-03-01 14:16:22 -0300

    Update functions

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # VirtualAssistantPython
+
+### Assistente virtual Cleiton, onde ao dizer o seu nome informa a hora, faz pesquisar no wikipedia e busca musicas no youtube.
+
+#### Iniciando o projeto
+* Crie o ambiente virtual 'venv' e ative
+* rode o comando no terminal dentro da raiz do projeto
+```python
+pip install -r requirements.txt
+```
+*  rode o arquivo app.py
+
+#### Correção para serem realizadas:
+[ ] Corrigir a linguagem
+
+[ ] Corrigir a velocidade da pronuncia 
+
+
+### OBS: Caso apresente erro com o pyaudio, verifique a correção de acordo com o arquivo correções pyaudio.
--- app.py
+++ app.py
@@ -1,24 +1,56 @@
 import speech_recognition as sr
 import pyttsx3
+from datetime import datetime
+import wikipedia as wk
+import pywhatkit as pw
+
 
 audio = sr.Recognizer()
 maquina = pyttsx3.init()
-maquina.say('Fala meu patrão')
-maquina.say('Eu sou o Cleiton')
-maquina.say('como posso ajudar?')
-maquina.runAndWait()
 
+def executa_comando():
 
+    try:
+        with sr.Microphone() as source:
+            print("Ouvindo...")
 
-try:
-    with sr.Microphone() as source:
-        print("Ouvindo...")
-        voz = audio.listen(source)
-        
-        comando = audio.recognize_google(voz, language='pt-BR')
-        comando = comando.lower()
-        if 'cleiton' in comando:
-            print(comando)
+            voz = audio.listen(source)
             
-except:
-    print('Microfone não esta OK')
+            comando = audio.recognize_google(voz, language='pt-BR')
+            comando = comando.lower()
+            
+            if 'cleiton' in comando:
+                comando = comando.replace('cleiton', '')
+                #maquina.say(comando)
+                maquina.runAndWait()       
+                print(comando)
+    except:
+        print('Microfone não esta OK')
+        
+    return comando
+    
+    
+def comando_voz_usuario():
+    comando = executa_comando()
+    
+    if 'horas' in comando:
+        hora = datetime.now().strftime('%H%M')
+        maquina.say('Agora são' + hora)
+        maquina.runAndWait()
+    elif 'procure por' in comando:
+        procurar = comando.replace('procure por', '')
+        wk.set_lang('pt')
+        resultado = wk.summary(procurar, 2)
+        print(resultado)
+        maquina.say(resultado)
+        maquina.runAndWait()
+    elif 'toque' in comando:
+        musica = comando.replace('toque', '')
+        resultado = pw.playonyt(musica)
+        maquina.say('Tocando musica')
+        maquina.runAndWait()
+        
+        
+        
+        
+comando_voz_usuario()
